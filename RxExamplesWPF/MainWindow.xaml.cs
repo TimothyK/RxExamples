5c23f77a57bee5dd5e0948db634194c59151d89a
Timothy Klenke <timothy.klenke@gmail.com>
2014-07-08 21:26:35 -0600

    Basic non-Rx button click handling

---
+++ RxExamplesWPF/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+
+namespace RxExamplesWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            LogBox.Text += DateTime.Now.ToString("HH:mm:ss.fffffff") + " - Click\r\n";
+            LogBox.ScrollToEnd();
+        }
+    }
+}
