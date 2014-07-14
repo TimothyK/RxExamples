992b5c55f98a6f6bb0c22fe2ed5c1766ae56ee1c
Timothy Klenke <timothy.klenke@gmail.com>
2014-07-13 22:55:05 -0600

    Route Through IObservable Subject

--- RxExamplesWPF/MainWindow.xaml.cs
+++ RxExamplesWPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Windows;
 
 namespace RxExamplesWPF
@@ -8,9 +10,13 @@ namespace RxExamplesWPF
     /// </summary>
     public partial class MainWindow
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            InitializeSubject();            
         }
 
         private void Button1_Click(object sender, RoutedEventArgs e)
@@ -18,5 +24,38 @@ namespace RxExamplesWPF
             LogBox.Text += DateTime.Now.ToString("HH:mm:ss.fffffff") + " - Click\r\n";
             LogBox.ScrollToEnd();
         }
+
+        #region Subject
+
+        private Subject<RoutedEventArgs> _subject;
+
+        private void InitializeSubject()
+        {
+            _subject = new Subject<RoutedEventArgs>();
+
+            _subject.Subscribe(Subject_Next);
+        }
+
+        private void Subject_Next(RoutedEventArgs e)
+        {
+            LogBox.Text += DateTime.Now.ToString("HH:mm:ss.fffffff") + " - Next Subject\r\n";
+            LogBox.ScrollToEnd();
+        }
+
+        private void SubjectButton_Click(object sender, RoutedEventArgs e)
+        {
+            _subject.OnNext(e);
+        }
+
+        #endregion
+
+        
+
+        
+
+
+
+
+
     }
 }
