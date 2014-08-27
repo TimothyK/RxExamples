4575702831060f029d23c14864acebf12e4c8835
Timothy Klenke <timothy.klenke@gmail.com>
2014-08-26 22:37:46 -0600

    Move ExceptionEqualityComparer to its own file

---
+++ RxExamples/NotificationPatterns/ExceptionEqualityComparer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RxExamples.NotificationPatterns
+{
+    class ExceptionEqualityComparer : IEqualityComparer<Exception>
+    {
+        public bool Equals(Exception x, Exception y)
+        {
+            if ((x == null) && (y == null)) return true;
+            if (x == null) return false;
+            if (y == null) return false;
+
+            if (x.GetType() != y.GetType())
+                return false;
+
+            if (x.StackTrace != y.StackTrace)
+                return false;
+
+            return Equals(x.InnerException, y.InnerException);
+        }
+
+        public int GetHashCode(Exception obj)
+        {
+            unchecked
+            {
+                return (obj.StackTrace.GetHashCode()*397 ^ obj.GetType().GetHashCode());
+            }
+        }
+    }
+}
--- RxExamples/NotificationPatterns/UnexpectedErrorsGrouped.cs
+++ RxExamples/NotificationPatterns/UnexpectedErrorsGrouped.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 
@@ -60,31 +59,5 @@ namespace RxExamples.NotificationPatterns
 
         }
 
-    }
-
-    class ExceptionEqualityComparer : IEqualityComparer<Exception>
-    {
-        public bool Equals(Exception x, Exception y)
-        {
-            if ((x == null) && (y == null)) return true;
-            if (x == null) return false;
-            if (y == null) return false;
-
-            if (x.GetType() != y.GetType())
-                return false;
-
-            if (x.StackTrace != y.StackTrace)
-                return false;
-
-            return Equals(x.InnerException, y.InnerException);
-        }
-
-        public int GetHashCode(Exception obj)
-        {
-            unchecked
-            {
-                return (obj.StackTrace.GetHashCode()*397 ^ obj.GetType().GetHashCode());
-            }
-        }
     }
 }
